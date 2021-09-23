f216f6bfbda310167003d9a3f249ce7bd456c842
MishkoM <83876790+MishkoM@users.noreply.github.com>
2021-09-23 15:17:57 +0200

    Delete demo directory

--- demo/src/main/java/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
