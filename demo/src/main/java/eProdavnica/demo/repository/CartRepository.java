5d218b6ef3c72a4e30454ae2e528728c4ef6e0f1
MishkoM <83876790+MishkoM@users.noreply.github.com>
2021-10-01 22:05:59 +0200

    Delete demo directory

--- demo/src/main/java/eProdavnica/demo/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package eProdavnica.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import eProdavnica.demo.model.Cart;
-
-public interface CartRepository extends CrudRepository<Cart, Integer>{
-
-}
--- demo/src/main/java/eProdavnica/demo/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package eProdavnica.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import eProdavnica.demo.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
